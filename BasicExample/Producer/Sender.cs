67c099166ce6d2619c4cc5cebd4e7025c63532a3
Gabriel Canosa <57040777+GabrielCanosa@users.noreply.github.com>
2023-05-24 13:12:13 +0200

    Add files via upload

---
+++ BasicExample/Producer/Sender.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+public class Sender
+{
+    private static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        {
+            using (var channel  = connection.CreateModel())
+            {
+                channel.QueueDeclare("BasicTestQueue", false, false, false, null);
+
+                string message = "Hola! soy un mensaje de RabbitMQ";
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("", "BasicTestQueue", null, body);
+
+                Console.WriteLine($"Sent message: {message}");
+            }
+        }
+    }
+}
