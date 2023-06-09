10eb8372b8dd13d5dd3c77c7971c59e31cf4729a
Gabriel Canosa <57040777+GabrielCanosa@users.noreply.github.com>
2023-06-09 12:19:15 +0200

    Add files via upload

---
+++ RabbitMQ.Polling/Consumer/Receiver.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var cts = new CancellationTokenSource();
+var token = cts.Token;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    BasicGetResult result;
+    while (true)
+    {
+        result = BasicGetMessage(channel);
+        Thread.Sleep(2000);
+    }
+}
+
+static BasicGetResult BasicGetMessage(IModel channel)
+{
+    BasicGetResult result = channel.BasicGet("PollingTestQueue", true);
+    if (result == null)
+        Console.WriteLine("There are no message in the queue");
+    else
+    {
+        var body = result.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine($"Received message: **  {message}  **");
+    }
+
+    return result;
+}
---
+++ RabbitMQ.Polling/Producer/Sender.cs
@@ -0,0 +1,19 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using (var connection = factory.CreateConnection())
+{
+    using (var channel = connection.CreateModel())
+    {
+        channel.QueueDeclare("PollingTestQueue", false, false, false, null);
+
+        string message = "Hola! soy un mensaje de RabbitMQ";
+
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish("", "PollingTestQueue", null, body);
+
+        Console.WriteLine($"Sent message: {message}");
+    }
+}
