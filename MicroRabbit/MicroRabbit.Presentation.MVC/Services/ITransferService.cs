d6790a1af09970c3735f4ccd364a7979cd84ba83
Gabriel Canosa <57040777+GabrielCanosa@users.noreply.github.com>
2023-06-15 12:17:55 +0200

    Add files via upload

---
+++ MicroRabbit/MicroRabbit.Presentation.MVC/Services/ITransferService.cs
@@ -0,0 +1,9 @@
+﻿using MicroRabbit.Presentation.MVC.Models.DTOs;
+
+namespace MicroRabbit.Presentation.MVC.Services
+{
+    public interface ITransferService
+    {
+        Task TransferDtoAsync(TransferDto transferDto);
+    }
+}
---
+++ MicroRabbit/MicroRabbit.Presentation.MVC/Services/TransferService.cs
@@ -0,0 +1,25 @@
+﻿using MicroRabbit.Presentation.MVC.Models.DTOs;
+using Newtonsoft.Json;
+
+namespace MicroRabbit.Presentation.MVC.Services
+{
+    public class TransferService : ITransferService
+    {
+        private readonly HttpClient _httpClient;
+
+        public TransferService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task TransferDtoAsync(TransferDto transferDto)
+        {
+            var response = await _httpClient.PostAsync("https://localhost:7045/api/banking", 
+                                                        new StringContent(JsonConvert.SerializeObject(transferDto), 
+                                                                          System.Text.Encoding.UTF8, 
+                                                                          "application/json")
+                                                        );
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
