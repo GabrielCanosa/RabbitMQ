af0147a51d2d86ea7b4d8749c350624e43516377
Gabriel Canosa <57040777+GabrielCanosa@users.noreply.github.com>
2023-06-15 12:15:23 +0200

    Add files via upload

---
+++ MicroRabbit/MicroRabbit.Presentation.MVC/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using MicroRabbit.Presentation.MVC.Models;
+using MicroRabbit.Presentation.MVC.Models.DTOs;
+using MicroRabbit.Presentation.MVC.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace MicroRabbit.Presentation.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ITransferService _transferService;
+
+        public HomeController(ILogger<HomeController> logger, ITransferService transferService)
+        {
+            _logger = logger;
+            _transferService = transferService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Transfer(TransferViewModel transferViewModel)
+        {
+            TransferDto transferDto = new TransferDto()
+            {
+                FromAccount = transferViewModel.FromAccount,
+                ToAccount = transferViewModel.ToAccount,
+                TransferAmount = transferViewModel.TransferAmount
+            };
+
+            await _transferService.TransferDtoAsync(transferDto);
+
+            return View("Index");
+        }
+    }
+}
---
+++ MicroRabbit/MicroRabbit.Presentation.MVC/Models/DTOs/TransferDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MicroRabbit.Presentation.MVC.Models.DTOs
+{
+    public class TransferDto
+    {
+        public int FromAccount { get; set; }
+        public int ToAccount { get; set; }
+        public decimal TransferAmount { get; set; }
+    }
+}
